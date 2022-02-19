f12add534f6bb5ff60636eeae42c96232399f453
Michael Glover <mglover1@my.athens.edu>
2022-02-19 15:24:49 -0600

    New faster Duplicate Prevention Solution

--- Convex Hull.cpp
+++ Convex Hull.cpp
@@ -41,8 +41,8 @@ std::vector<Point> convexHull(std::vector<Point>& pointsVector) {
             }
             /* Thought here what if we just push back the starting node and at the start push the first node onto the end of the vector so it checks them all? maybe*/
             if (exteriorPoint == true) {
-                if(std::find(hull.begin(),hull.end(),*i) == hull.end())
-                    hull.push_back(*i);
+                j = pointsVector.end() - 1;                     //if it finds an edge solution involving the node i it does not continue the search as the other side will be found later
+                hull.push_back(*i);
             }
            
         }
@@ -60,16 +60,7 @@ int main()
     for (auto i = out.begin(); i < out.end(); i++) {
         std::cout << i->first << "," << i->second << std::endl;
     }
-    std::cout << "Hello World!\n";
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
